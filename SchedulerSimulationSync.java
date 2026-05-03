4af9cdaa167773b3e6e0e26ca17705dc73cd4dcb
yousef-ayiedh444 <444051021@std.psau.edu.sa>
2026-05-03 05:41:09 +0300

    protect total waiting time using reentrantlock

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -72,8 +72,13 @@ class SharedResources {
     
     // Method to add waiting time
     public static void addWaitingTime(long time) {
-        // TODO: Protect this critical section with a lock
+        // TODO: Protect this critical section with a lock 
+        counterLock.lock();
+        try {
         totalWaitingTime += time;
+    } finally {
+        counterLock.unlock();
+    }
     }
     
     // Method to log execution
