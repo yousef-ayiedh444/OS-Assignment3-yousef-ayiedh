d876de650b4cc0956ba6d12ae6b8ee146a75a4a6
yousef-ayiedh444 <444051021@std.psau.edu.sa>
2026-05-03 06:21:24 +0300

    use semaphore to control cpu access in run method and we release it in finally

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -121,8 +121,13 @@ class Process implements Runnable {
     public void run() {
         // TODO #3: Acquire CPU semaphore before executing
         // This ensures only allowed number of processes run simultaneously
-        
+        //ACQUIRE CPU ACCESS BEFORE EXECUTING
         try {
+            SharedResources.cpuSemaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }   
+         try {
             if (startTime == -1) {
                 startTime = System.currentTimeMillis();
             }
@@ -183,7 +188,8 @@ class Process implements Runnable {
             
         } finally {
             // TODO #4: Release CPU semaphore here
-            // Always release in finally block to prevent deadlocks!
+            // Always release in finally block to prevent deadlocks! 
+            SharedResources.cpuSemaphore.release(); 
         }
     }
     
